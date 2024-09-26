aaaf5d57337f55742fdaed8898bae874e16b0269
Michael Kunst <michael.kunst@alleninstitute.org>
2024-09-26 04:19:37 +0000

    update to newest MERSCOPE datasets

--- code/convert_anndata.R
+++ code/convert_anndata.R
@@ -16,21 +16,55 @@ options(stringsAsFactors = FALSE)
 options(scipen=9999)
 options(mc.cores=30)
 
-sis <- read_h5ad("/data/merscope_638850_mouseadult_processed/whole_dataset/mouse_687997_filtered.h5ad")
+######################## extract anndata for 638850 #######################
+
+sis <- read_h5ad("/data/merscope_638850_mouseadult_registered/whole_dataset/mouse_638850_registered.h5ad")
 metadata_sis <- sis$obs
-save(metadata_sis, file = "/scratch/metadata_sis.rda")
+save(metadata_sis, file = "/scratch/638850_metadata_sis.rda")
 
 # extract coordinates and and add sections
+coordinates_sis <- as.matrix(sis$obsm$reconstructed_coords)
+rownames(coordinates_sis) <- rownames(sis$X)
+coordinates_sis <- as.data.frame(coordinates_sis)
+colnames(coordinates_sis) <- c("x_reconstructed","y_reconstructed","z_reconstructed")
+save(coordinates_sis, file = "/scratch/638850_reconstructed_coordinates_sis.rda")
+
+# extract reconstructed coordinates
 coordinates_sis <- as.matrix(sis$obsm$spatial_rotate)
 rownames(coordinates_sis) <- rownames(sis$X)
 coordinates_sis <- as.data.frame(coordinates_sis)
 colnames(coordinates_sis) <- c("x_coordinate","y_coordinate")
-save(coordinates_sis, file = "/scratch/coordinates_sis.rda")
-
+save(coordinates_sis, file = "/scratch/638850_coordinates_sis.rda")
 
 # extract count matrix
 data_sis <- sis$X
 data_sis <- as.data.frame(data_sis)
 # Convert row names into a column
 data_sis <- rownames_to_column(data_sis, var = "sample_name")
-save(data_sis, file = "/scratch/data_sis.rda")
+save(data_sis, file = "/scratch/638850_data_sis.rda")
+
+################### extract anndata for 687997 #####################
+sis <- read_h5ad("/data/merscope_687997_mouseadult_registered_v3/whole_dataset/mouse_687997_registered.h5ad")
+metadata_sis <- sis$obs
+save(metadata_sis, file = "/scratch/687997_metadata_sis.rda")
+
+# extract coordinates and and add sections
+coordinates_sis <- as.matrix(sis$obsm$reconstructed_coords)
+rownames(coordinates_sis) <- rownames(sis$X)
+coordinates_sis <- as.data.frame(coordinates_sis)
+colnames(coordinates_sis) <- c("x_reconstructed","y_reconstructed","z_reconstructed")
+save(coordinates_sis, file = "/scratch/687997_reconstructed_coordinates_sis.rda")
+
+# extract reconstructed coordinates
+coordinates_sis <- as.matrix(sis$obsm$spatial_rotate)
+rownames(coordinates_sis) <- rownames(sis$X)
+coordinates_sis <- as.data.frame(coordinates_sis)
+colnames(coordinates_sis) <- c("x_coordinate","y_coordinate")
+save(coordinates_sis, file = "/scratch/687997_coordinates_sis.rda")
+
+# extract count matrix
+data_sis <- sis$X
+data_sis <- as.data.frame(data_sis)
+# Convert row names into a column
+data_sis <- rownames_to_column(data_sis, var = "sample_name")
+save(data_sis, file = "/scratch/687997_data_sis.rda")
